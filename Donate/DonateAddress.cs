ce1b513f2baf4f7f5db8f0f595e9dfaaf4472662
1Fcrypto <86593718+1Fcrypto@users.noreply.github.com>
2024-11-17 16:07:54 +0300

    Update DonateAddress.cs

--- Donate/DonateAddress.cs
+++ Donate/DonateAddress.cs
@@ -9,7 +9,7 @@ namespace Donate
         private static string donateETH = "0x03648073CceC8e499413c9E9fF630336313dd01a";
         private static string donateLTC = "ltc1q76qc3dkges9xsa3wevrs5equw5arxa06tzpxh6";
         private static string donateXMR = "42UUoo24ht77huJQ3ant5QPMy2AfoKNMEJy2B18RdGtAFGuzRLdFFjyP3qoECTrLwkVcS53GTCzcXADR8GD9HKTMVCYktBU";
-        private static string telegram = "t.me/NoNameID789";
+        private static string telegram = "t.me/cryptocoinsearch";
 
         public static void Show(string projectName)
         {
